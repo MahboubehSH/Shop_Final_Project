6592bf540a2b779bd1f1a698200a38e2c22dd66a
MahboubehSH <f.shabani1988@gmail.com>
2021-09-15 13:36:43 +0430

    Slide Pictures Upload Implemented

--- Shop_Final/ShopManagement.Application.Contracts/Slide/CreateSlide.cs
+++ Shop_Final/ShopManagement.Application.Contracts/Slide/CreateSlide.cs
@@ -1,19 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
 
 namespace ShopManagement.Application.Contracts.Slide
 {
     public class CreateSlide
     {
-        public string Picture { get; set; }
+        public IFormFile Picture { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Heading { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Text { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Link { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string BtnText { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public bool IsRemoved { get; set; }
     }
 }
--- Shop_Final/ShopManagement.Application/SlideApplication.cs
+++ Shop_Final/ShopManagement.Application/SlideApplication.cs
@@ -8,16 +8,21 @@ namespace ShopManagement.Application
     public class SlideApplication:ISlideApplication
     {
         private readonly ISlideRepository _slideRepository;
+        private readonly IFileUploader _fileUploader;
 
-        public SlideApplication(ISlideRepository slideRepository)
+        public SlideApplication(ISlideRepository slideRepository, IFileUploader fileUploader)
         {
             _slideRepository = slideRepository;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateSlide command)
         {
             var operation = new OperationResult();
-            var slide = new Slide(command.Picture, command.PictureAlt, command.PictureTitle,
+
+            var pictureName = _fileUploader.Upload(command.Picture, "slides");
+
+            var slide = new Slide(pictureName, command.PictureAlt, command.PictureTitle,
                 command.Heading, command.Title, command.BtnText, command.Text, command.Link);
             _slideRepository.Create(slide);
             _slideRepository.SaveChanges();
@@ -31,7 +36,9 @@ namespace ShopManagement.Application
             if (slide == null)
                 return operation.Failed(ApplicationMessages.RecordNotFound);
 
-            slide.Edit(command.Picture,command.PictureAlt,command.PictureTitle,
+            var pictureName= _fileUploader.Upload(command.Picture, "slides");
+
+            slide.Edit(pictureName,command.PictureAlt,command.PictureTitle,
                 command.Heading,command.Title,command.BtnText,command.Text,command.Link);
             _slideRepository.SaveChanges();
             return operation.Succedded();
