e5529365fc9a65e477a6fb1acd8d4b5f9f512f3e
MahboubehSH <f.shabani1988@gmail.com>
2021-09-22 10:41:58 +0330

    Article Category Implemented; Article Domain & Application Implemented

---
+++ Shop_Final/BlogManagement.Domain/ArticleAgg/IArticleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _0_Framework.Domain;
+using BlogManagement.Application.Contracts.Article;
+
+namespace BlogManagement.Domain.ArticleAgg
+{
+    public interface IArticleRepository:IRepository<long , Article>
+    {
+        EditArticle GetDetails(long id);
+        Article GetWithCategory(long id);
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
