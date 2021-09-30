4d5785a2ae5c60cdbc72d0b87345316794560bab
MahboubehSH <f.shabani1988@gmail.com>
2021-09-30 11:28:56 +0330

    Account Authentication Implemented

--- Shop_Final/0_Framework/Application/ApplicationMessages.cs
+++ Shop_Final/0_Framework/Application/ApplicationMessages.cs
@@ -8,5 +8,7 @@ namespace _0_Framework.Application
     {
         public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد.لطفا مجددا تلاش بفرمائید";
         public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد.لطفا مجددا تلاش بفرمائید";
+        public const string PasswordsNotMath = "پسورد و تکرار آن با هم مطابقت ندارند";
+        public const string WrongUserPass = "نام کاربری یا کلمه رمز اشتباه است";
     }
 }
